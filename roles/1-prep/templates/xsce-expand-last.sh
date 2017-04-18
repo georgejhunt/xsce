5257e43393736806bf4334af8bda6e687404e1cf
George Hunt <georgejhunt@gmail.com>
2017-04-18 16:14:32 -0400

    turn of expand flag, skip over if not enabled

--- roles/1-prep/templates/xsce-expand-last.sh
+++ roles/1-prep/templates/xsce-expand-last.sh
@@ -1,9 +1,12 @@
 #!/bin/bash 
 # script to resize the last partition
 
-LAST=`fdisk -l | grep /dev/sda | tail -n1 | cut -d' ' -f1`
-LAST_NO=${LAST: (-1)}
-DEVICE=${LAST:0:-1}
+if [  -f /.resize-rootfs ];then
+	LAST=`fdisk -l | grep /dev/sda | tail -n1 | cut -d' ' -f1`
+	LAST_NO=${LAST: (-1)}
+	DEVICE=${LAST:0:-1}
 
-parted -s -f $DEVICE resizepart $LAST_NO 100%
-resize2fs $LAST
+	parted -s -f $DEVICE resizepart $LAST_NO 100%
+	resize2fs $LAST
+        rm /.resize-rootfs
+fi
